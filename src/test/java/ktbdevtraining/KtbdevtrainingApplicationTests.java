0805f5cbb5200c71716a2a331bb7d0737a639aa7
vegasl <63402313+vegasl@users.noreply.github.com>
2020-04-10 14:40:09 +0700

    Initial commit

---
+++ src/test/java/ktbdevtraining/KtbdevtrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package ktbdevtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbdevtrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
