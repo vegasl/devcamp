c7427881b540376fd6925e76889084da482ed075
vegasl <63402313+vegasl@users.noreply.github.com>
2020-04-13 15:56:21 +0700

    Add

---
+++ Mission02/src/test/java/Mission02/Mission02ApplicationTests.java
@@ -0,0 +1,13 @@
+package Mission02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mission02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
