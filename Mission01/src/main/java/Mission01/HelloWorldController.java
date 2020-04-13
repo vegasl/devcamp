21cfb0d2d3a7b10dbba92a0a3a1d2bde4934e3cf
vegasl <63402313+vegasl@users.noreply.github.com>
2020-04-13 16:13:07 +0700

    Add

--- Mission01/src/main/java/Mission01/HelloWorldController.java
+++ Mission01/src/main/java/Mission01/HelloWorldController.java
@@ -1,4 +1,4 @@
-package ktbdevtraining;
+package Mission01;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
