90d41ff8995aa5d829bee643c9292da7f5724e84
vegasl <63402313+vegasl@users.noreply.github.com>
2020-04-15 19:06:36 +0700

    Update

--- Mission02/src/main/java/mission02/Mission02Application.java
+++ Mission02/src/main/java/mission02/Mission02Application.java
@@ -2,10 +2,8 @@ package mission02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("mission02.doggo.DoggoRepository")
 public class Mission02Application {
 
 	public static void main(String[] args) {
